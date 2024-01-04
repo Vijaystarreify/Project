4d1138164688a6c30fe943dee06652d7ad0be8ac
Vijay <vc892782@gmail.com>
2024-01-04 19:31:29 +0530

    routeschanges

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-	
+      //movie Routes
 	Route::get('/movies', function () {
 		return view('admin.movies-list');
 	})->name('movies-list');
-    //movie controller
 	Route::get('/movie/{id}/form',[MovieController::class,'movieForm'])->name('movie-form');
 	Route::get('/movie/{id}/delete',[MovieController::class,'deleteMovie'])->name('movie-delete');
 	Route::post('/movie/save',[MovieController::class,'saveMovieData'])->name('save-movie-data');
@@ -41,20 +40,18 @@ Route::middleware([
 Route::get('/theaters', function () {
     return view('admin.theaters-list');
 })->name('theaters-list');
-// Theater controller
 Route::get('/theater/{id}/form', [TheaterController::class, 'theaterForm'])->name('theater-form');
 Route::get('/theater/{id}/delete', [TheaterController::class, 'deleteTheater'])->name('theater-delete');
 Route::post('/theater/save', [TheaterController::class, 'saveTheaterData'])->name('save-theater-data');
 Route::get('/theater/{theaterId}/open-screen-modal', [TheaterController::class, 'openScreenModal'])->name('theater.open-screen-modal');
 Route::post('/add-screen', [TheaterController::class, 'addScreen'])->name('add-screen');
-Route::get('/get-screen-details/{screenId}', [TheaterController::class, 'getScreenDetails']);
+// Route::get('/get-screen-details/{screenId}', [TheaterController::class, 'getScreenDetails']);
 Route::post('/update-screen-capacity', [TheaterController::class, 'updateScreenCapacity'])->name('update-screen-capacity');
 
+// Show Routes
 Route::get('/shows', function () {
     return view('admin.show-list');
 })->name('shows-list');
-
-// Show controller
 Route::get('/show/{id}/form', [ShowController::class, 'showForm'])->name('show-form');
 Route::get('/show/{id}/delete', [ShowController::class, 'deleteShow'])->name('show-delete');
 Route::post('/show/save', [ShowController::class, 'saveShowData'])->name('save-show-data');
